e7426f3b329a16fa70b9ede46487ee7aba8e752a
Jeanderson Candido <jeandersonbc@gmail.com>
2014-04-03 03:34:56 -0300

    Refactoring code
    Defined hidden methods from the interface

--- src/model.py
+++ src/model.py
@@ -29,50 +29,6 @@ class AntColony:
             print "%3d" %trail[i],
         print "\n"
 
-    def move_probs(self, k, cityX, visited, pheromones, dists):
-        probs = [0.0 for i in range(self.__numCities)]
-        total = 0.0
-        upperBoundFactor = 1.7976931348623157E+308 / (self.__numCities * 100)
-        lowerBoundFactor = 0.0001
-        for i in range(len(probs)):
-            if i == cityX or visited[i]:
-                probs[i] = 0.0
-            else:
-                probs[i] = pow(pheromones[cityX][i], self.__alpha) \
-                            * pow((1.0 / self.distance(cityX, i, dists)), self.__beta)
-                if probs[i] < lowerBoundFactor:
-                    probs[i] = lowerBoundFactor
-                elif probs[i] > upperBoundFactor:
-                    probs[i] = upperBoundFactor
-            total += probs[i]
-
-        return [(probs[i] / total) for i in range(self.__numCities)]
-
-    def next_city(self, k, cityX, visited, pheromones, dists):
-        probs = self.move_probs(k, cityX, visited, pheromones, dists)
-        cumul = [0.0 for i in range(len(probs) + 1)]
-        for i in range(len(probs)):
-            cumul[i+1] = cumul[i] + probs[i]
-
-        p = random.random() 
-        for i in range(len(cumul)-1):
-            if (p >= cumul[i] and p < cumul[i+1]):
-                return i
-
-    def build_trail(self, k, start, pheromones, dists):
-        trail = [0 for i in range(self.__numCities)]
-        visited = [False for i in range(self.__numCities)]
-
-        trail[0] = start
-        visited[start] = True
-        for i in range(self.__numCities-1):
-            cityX = trail[i]
-            nextCity = self.next_city(k, cityX, visited, pheromones, dists)
-            trail[i+1] = nextCity
-            visited[nextCity] = True
-
-        return trail
-
     def init_pheromones(self):
         """ Initializes a matrix representing the pheromones. """
         pheromones = [
@@ -87,7 +43,7 @@ class AntColony:
     def update_ants(self, ants, pheromones, dists):
         for k in range(len(ants)):
             start = random.randint(0, self.__numCities-1)
-            newTrail = self.build_trail(k, start, pheromones, dists)
+            newTrail = self.__build_trail(k, start, pheromones, dists)
             ants[k] = newTrail
 
     def update_pheromones(self, pheromones, ants, dists):
@@ -97,7 +53,7 @@ class AntColony:
                     currLength = self.length(ants[k], dists)
                     decrease = (1.0 - self.__rho) * pheromones[i][j]
                     increase = 0.0
-                    if self.edge_in_trail(i, j, ants[k]):
+                    if self.__edge_in_trail(i, j, ants[k]):
                         increase = (self.__Q / currLength)
                     pheromones[i][j] = decrease + increase
                     if pheromones[i][j] < 0.001:
@@ -105,28 +61,6 @@ class AntColony:
                     elif pheromones[i][j] > 100000.0:
                         pheromones[i][j] = 100000.0
                     pheromones[j][i] = pheromones[i][j]
-
-    def edge_in_trail(self, cityX, cityY, trail):
-        lastIndex = len(trail) - 1
-        idx = trail.index(cityX)
-        if (idx == 0 and trail[1] == cityY) \
-                or (idx == 0 and trail[lastIndex] == cityY):
-            return True
-
-        elif idx == 0:
-            return False
-
-        elif (idx == lastIndex and trail[lastIndex - 1] == cityY) \
-                or (idx == lastIndex and trail[0] == cityY):
-            return True
-
-        elif idx == lastIndex:
-            return False
-
-        elif trail[idx - 1] == cityY or trail[idx + 1] == cityY:
-            return True
-
-        return False
 
     def best_trail(self, ants, dists):
         bestLength = self.length(ants[0], dists)
@@ -139,35 +73,18 @@ class AntColony:
 
         return ants[idxBestLen]
 
-    def distance(self, cityX, cityY, dists):
-        return dists[cityX][cityY]
-
     def length(self, trail, dists):
         result = 0.0
         for i in range(len(trail)-1):
-            result += self.distance(trail[i], trail[i+1], dists)
+            result += self.__distance(trail[i], trail[i+1], dists)
 
         return result
-
-    def random_trail(self, start):
-        trail = range(self.__numCities)
-
-        # Shuffle
-        for i in range(self.__numCities):
-            r = random.randint(i, self.__numCities-1)
-            trail[r], trail[i] = trail[i], trail[r]
-
-        # Start at 0
-        idx = trail.index(start)
-        trail[idx], trail[0] = trail[0], trail[idx]
-
-        return trail
 
     def init_ants(self):
         ants = [[] for r in range(self.__numAnts)]
         for k in range(self.__numAnts):
             start = random.randint(0, self.__numCities-1)
-            ants[k] = self.random_trail(start, self.__numCities)
+            ants[k] = self.__random_trail(start)
 
         return ants
 
@@ -193,3 +110,87 @@ class AntColony:
                 dists[i][j] = dists[j][i] = d
 
         return dists
+
+    def __move_probs(self, k, cityX, visited, pheromones, dists):
+        probs = [0.0 for i in range(self.__numCities)]
+        total = 0.0
+        upperBoundFactor = 1.7976931348623157E+308 / (self.__numCities * 100)
+        lowerBoundFactor = 0.0001
+        for i in range(len(probs)):
+            if i == cityX or visited[i]:
+                probs[i] = 0.0
+            else:
+                probs[i] = pow(pheromones[cityX][i], self.__alpha) \
+                            * pow((1.0 / self.__distance(cityX, i, dists)), self.__beta)
+                if probs[i] < lowerBoundFactor:
+                    probs[i] = lowerBoundFactor
+                elif probs[i] > upperBoundFactor:
+                    probs[i] = upperBoundFactor
+            total += probs[i]
+
+        return [(probs[i] / total) for i in range(self.__numCities)]
+
+    def __next_city(self, k, cityX, visited, pheromones, dists):
+        probs = self.__move_probs(k, cityX, visited, pheromones, dists)
+        cumul = [0.0 for i in range(len(probs) + 1)]
+        for i in range(len(probs)):
+            cumul[i+1] = cumul[i] + probs[i]
+
+        p = random.random() 
+        for i in range(len(cumul)-1):
+            if (p >= cumul[i] and p < cumul[i+1]):
+                return i
+
+    def __build_trail(self, k, start, pheromones, dists):
+        trail = [0 for i in range(self.__numCities)]
+        visited = [False for i in range(self.__numCities)]
+
+        trail[0] = start
+        visited[start] = True
+        for i in range(self.__numCities-1):
+            cityX = trail[i]
+            nextCity = self.__next_city(k, cityX, visited, pheromones, dists)
+            trail[i+1] = nextCity
+            visited[nextCity] = True
+
+        return trail
+
+    def __random_trail(self, start):
+        trail = range(self.__numCities)
+
+        # Shuffle
+        for i in range(self.__numCities):
+            r = random.randint(i, self.__numCities-1)
+            trail[r], trail[i] = trail[i], trail[r]
+
+        # Start at 0
+        idx = trail.index(start)
+        trail[idx], trail[0] = trail[0], trail[idx]
+
+        return trail
+
+    def __distance(self, cityX, cityY, dists):
+        return dists[cityX][cityY]
+
+    def __edge_in_trail(self, cityX, cityY, trail):
+        lastIndex = len(trail) - 1
+        idx = trail.index(cityX)
+        if (idx == 0 and trail[1] == cityY) \
+                or (idx == 0 and trail[lastIndex] == cityY):
+            return True
+
+        elif idx == 0:
+            return False
+
+        elif (idx == lastIndex and trail[lastIndex - 1] == cityY) \
+                or (idx == lastIndex and trail[0] == cityY):
+            return True
+
+        elif idx == lastIndex:
+            return False
+
+        elif trail[idx - 1] == cityY or trail[idx + 1] == cityY:
+            return True
+
+        return False
+
